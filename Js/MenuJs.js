205d9e09b0ea284671a26e352f5ccc6a7e380ea7
Jesus Vidal <Ramirez_vidal@outlook.com>
2022-01-21 23:13:26 -0500

    Agregando nuevas experiencias, proyectos al portafolio

--- Js/MenuJs.js
+++ Js/MenuJs.js
@@ -1,3 +1,32 @@
+const menu = document.querySelector(".menu");
 
-console.log('el escript desde otro archivo');
 
+const boton = document.querySelector(".Fig-menu");
+
+
+boton.addEventListener("click", Mostrar);
+
+const changes = window.matchMedia("screen and (max-width:767px)");
+
+changes.addListener(validacion);
+
+function validacion(event) {
+    // console.log(event);
+    if (event.matches) {
+        boton.addEventListener("click", Mostrar);
+
+    } else {
+        boton.removeEventListener("click", Mostrar);
+    }
+}
+
+function Mostrar() {
+    if (menu.classList.contains("Animacion-Menu")) {
+        menu.classList.remove("Animacion-Menu");
+        console.log("Mostrar quitado");
+    } else {
+        menu.classList.add("Animacion-Menu");
+        console.log("Mostrar abierto");
+    }
+}
+//Agregar clase/ menu.classList.add('Animacion-Menu');
